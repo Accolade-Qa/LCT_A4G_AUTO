b6493da2fd2cbd5dd30993fbda885c937e99ef3e
JDhananjay79 <jagtapdhananjay79@gmail.com>
2026-06-05 16:18:09 +0530

    user format repaired

--- tests/test_user_management.py
+++ tests/test_user_management.py
@@ -4,29 +4,7 @@ logger = get_logger(__name__)
 
 
 class TestUsermanagement:
-    # @pytest.fixture(autouse=True)
-    # def log_test_case(self, request):
-    #     test_name = request.node.name
-    #     logger.info("Starting SIM Batch Data Details test: %s", test_name)
-    #     logger.debug("Executing test node: %s", request.node.nodeid)
-    #     yield
-    #     report = getattr(request.node, "rep_call", None)
-    #     if report is None:
-    #         logger.debug(
-    #             "SIM Batch Data Details test finished without call report: %s",
-    #             test_name,
-    #         )
-    #     elif report.passed:
-    #         logger.info("SIM Batch Data Details test passed: %s", test_name)
-    #     elif report.failed:
-    #         logger.error("SIM Batch Data Details test failed: %s", test_name)
-    #         logger.debug(
-    #             "SIM Batch Data Details failure details for %s: %s",
-    #             test_name,
-    #             report.longrepr,
-    #         )
-    #     elif report.skipped:
-    #         logger.warning("SIM Batch Data Details test skipped: %s", test_name)
+    
 
     def test_user_management_nav_list_enability(self, user_management, report_case):
         enabled = user_management._nav_list_enability()
@@ -63,84 +41,84 @@ class TestUsermanagement:
 
 
 
-def test_first_name_field(user_management):
+    def test_first_name_field(self, user_management):
 
-    result = user_management.first_name_field()
+        result = user_management.first_name_field()
 
-    assert result["name_blank_text"] == "This field is required and can't be empty."
-    assert result["name_num_text"] == "Only alphabets and spaces are allowed."
-    assert result["name_sp_char_text"] == "Only alphabets and spaces are allowed."
-    assert result["name_space_text"] == "This field is required and can't be only spaces."
-    assert result["name_char_space_text"] == "Remove leading or trailing spaces."
+        assert result["name_blank_text"] == "This field is required and can't be empty."
+        assert result["name_num_text"] == "Only alphabets and spaces are allowed."
+        assert result["name_sp_char_text"] == "Only alphabets and spaces are allowed."
+        assert result["name_space_text"] == "This field is required and can't be only spaces."
+        assert result["name_char_space_text"] == "Remove leading or trailing spaces."
     
 
-def test_last_name_field(user_management):
+    def test_last_name_field(self, user_management):
 
-    result = user_management.last_name_field()
+        result = user_management.last_name_field()
 
-    assert result["last_name_blank_text"] == "This field is required and can't be empty."
-    assert result["last_name_num_text"] == "Only alphabets and spaces are allowed."
-    assert result["last_name_sp_char_text"] == "Only alphabets and spaces are allowed."
-    assert result["last_name_space_text"] == "This field is required and can't be only spaces."
-    assert result["last_name_char_space_text"] == "Remove leading or trailing spaces."
-
-
-def test_email_field(user_management):
-
-    result = user_management.email_field()
-
-    assert result["email_blank_text"] == "This field is required and can't be empty."
-    assert result["email_num_text"] == "Please enter a valid Email ID."
-    assert result["email_sp_char_text"] == "Please enter a valid Email ID."
-    assert result["email_space_text"] == "This field is required and can't be only spaces."
-    assert result["email_char_space_text"] == "Please enter a valid Email ID."
-
-def test_mob_no_field(user_management):
-
-    result = user_management.mob_no_field()
-
-    assert result["mob_blank_text"] == "Mobile number is required."
-    assert result["mob_num_text"] == "Enter a valid mobile number."
-    assert result["mob_sp_char_text"] == "Enter a valid mobile number."
-    assert result["mob_space_text"] == "Mobile number is required."
-    assert result["mob_char_space_text"] == "Enter a valid mobile number."
-
-def test_country_field(user_management):
-
-    result = user_management.country_field()
-
-    assert result["con_blank_text"] == "This field is required and can't be empty."
-    assert result["con_num_text"] == "Only alphabets and spaces are allowed."
-    assert result["con_sp_char_text"] == "Only alphabets and spaces are allowed."
-    assert result["con_space_text"] == "This field is required and can't be only spaces."
-    assert result["con_char_space_text"] == "Remove leading or trailing spaces."
-
-def test_state_field(user_management):
-
-    result = user_management.state_field()
-
-    assert result["state_blank_text"] == "This field is required and can't be empty."
-    assert result["state_num_text"] == "Only alphabets and spaces are allowed."
-    assert result["state_sp_char_text"] == "Only alphabets and spaces are allowed."
-    assert result["state_space_text"] == "This field is required and can't be only spaces."
-    assert result["state_char_space_text"] == "Remove leading or trailing spaces."
+        assert result["last_name_blank_text"] == "This field is required and can't be empty."
+        assert result["last_name_num_text"] == "Only alphabets and spaces are allowed."
+        assert result["last_name_sp_char_text"] == "Only alphabets and spaces are allowed."
+        assert result["last_name_space_text"] == "This field is required and can't be only spaces."
+        assert result["last_name_char_space_text"] == "Remove leading or trailing spaces."
 
 
-def test_usermanagement_status_field(user_management):
+    def test_email_field(self, user_management):
 
-    error_msg = user_management.status_field()
+        result = user_management.email_field()
 
-    assert (
-        error_msg["status_locator_text"] == "This field is required and can't be empty."
-    ), f"Expected error message to be 'This field is required and can't be empty.', got '{error_msg["status_locator_text"]}'"
+        assert result["email_blank_text"] == "This field is required and can't be empty."
+        assert result["email_num_text"] == "Please enter a valid Email ID."
+        assert result["email_sp_char_text"] == "Please enter a valid Email ID."
+        assert result["email_space_text"] == "This field is required and can't be only spaces."
+        assert result["email_char_space_text"] == "Please enter a valid Email ID."
 
-def test_usermanagement_new_flow(user_management):
+    def test_mob_no_field(self, user_management):
+
+        result = user_management.mob_no_field()
+
+        assert result["mob_blank_text"] == "Mobile number is required."
+        assert result["mob_num_text"] == "Enter a valid mobile number."
+        assert result["mob_sp_char_text"] == "Enter a valid mobile number."
+        assert result["mob_space_text"] == "Mobile number is required."
+        assert result["mob_char_space_text"] == "Enter a valid mobile number."
+
+    def test_country_field(self, user_management):
+
+        result = user_management.country_field()
+
+        assert result["con_blank_text"] == "This field is required and can't be empty."
+        assert result["con_num_text"] == "Only alphabets and spaces are allowed."
+        assert result["con_sp_char_text"] == "Only alphabets and spaces are allowed."
+        assert result["con_space_text"] == "This field is required and can't be only spaces."
+        assert result["con_char_space_text"] == "Remove leading or trailing spaces."
+
+    def test_state_field(self, user_management):
+
+        result = user_management.state_field()
+
+        assert result["state_blank_text"] == "This field is required and can't be empty."
+        assert result["state_num_text"] == "Only alphabets and spaces are allowed."
+        assert result["state_sp_char_text"] == "Only alphabets and spaces are allowed."
+        assert result["state_space_text"] == "This field is required and can't be only spaces."
+        assert result["state_char_space_text"] == "Remove leading or trailing spaces."
+
+
+    def test_usermanagement_status_field(self, user_management):
+
+        error_msg = user_management.status_field()
+
+        assert (
+            error_msg["status_locator_text"] == "This field is required and can't be empty."
+        ), f"Expected error message to be 'This field is required and can't be empty.', got '{error_msg["status_locator_text"]}'"
+
+    def test_usermanagement_new_flow(self, user_management):
     
-    user_management.new_flow()
+        user_management.new_flow()
     
-def test_usermanagement_update_flow(user_management):
+    def test_usermanagement_update_flow(self, user_management):
     
-    user_management.update_flow()
+        user_management.update_flow()
     
         
     
